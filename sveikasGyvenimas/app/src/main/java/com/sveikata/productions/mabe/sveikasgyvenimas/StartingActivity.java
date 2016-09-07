7906575c4d3b603da5c0d610639c66e99c4c9330
martynux123 <dargis.martynas@gmail.com>
2016-09-07 20:19:26 +0300

    starting activity

---
+++ sveikasGyvenimas/app/src/main/java/com/sveikata/productions/mabe/sveikasgyvenimas/StartingActivity.java
@@ -0,0 +1,24 @@
+package com.sveikata.productions.mabe.sveikasgyvenimas;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class StartingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starting);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(StartingActivity.this, LoginActivity.class));
+            }
+        }, 2000L);
+    }
+}
