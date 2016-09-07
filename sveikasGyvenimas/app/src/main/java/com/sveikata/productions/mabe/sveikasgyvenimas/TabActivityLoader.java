ae3b7168af5137d3d59997d1c2ed8bbacaa9743f
pijoneris <benasmiliunas@gmail.com>
2016-09-07 19:55:12 +0300

    m

---
+++ sveikasGyvenimas/app/src/main/java/com/sveikata/productions/mabe/sveikasgyvenimas/TabActivityLoader.java
@@ -0,0 +1,46 @@
+package com.sveikata.productions.mabe.sveikasgyvenimas;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class TabActivityLoader extends AppCompatActivity {
+
+    int[] tabIcons = {R.drawable.schedule, R.drawable.play,R.drawable.facts,R.drawable.questions};
+    public static String[] tabs = {"Schedule", "Play", ""};
+    private ViewPager viewPager;
+    public static TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab_activity_loader);
+
+        tabLayout = (TabLayout) findViewById(R.id.my_tab_layout);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager(), getApplicationContext()));
+        viewPager.getCurrentItem();
+
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+    }
+}
