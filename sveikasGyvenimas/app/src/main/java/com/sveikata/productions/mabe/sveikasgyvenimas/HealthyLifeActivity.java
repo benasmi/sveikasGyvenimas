546baa0bec0d2a44253a1582e6425b967aeef583
martynux123 <dargis.martynas@gmail.com>
2016-09-10 22:52:43 +0300

    google maps fragment added

--- sveikasGyvenimas/app/src/main/java/com/sveikata/productions/mabe/sveikasgyvenimas/HealthyLifeActivity.java
+++ sveikasGyvenimas/app/src/main/java/com/sveikata/productions/mabe/sveikasgyvenimas/HealthyLifeActivity.java
@@ -1,11 +1,15 @@
 package com.sveikata.productions.mabe.sveikasgyvenimas;
 
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.maps.SupportMapFragment;
 
 public class HealthyLifeActivity extends android.support.v4.app.Fragment {
 
@@ -14,6 +18,20 @@ public class HealthyLifeActivity extends android.support.v4.app.Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_healthy_life,container,false);
 
+
+
         return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        FragmentManager fm = getChildFragmentManager();
+        Fragment fragment = (SupportMapFragment) fm.findFragmentById(R.id.map_container);
+        if (fragment == null) {
+            fragment = SupportMapFragment.newInstance();
+            fm.beginTransaction().replace(R.id.map_container, fragment).commit();
+        }
     }
 }
