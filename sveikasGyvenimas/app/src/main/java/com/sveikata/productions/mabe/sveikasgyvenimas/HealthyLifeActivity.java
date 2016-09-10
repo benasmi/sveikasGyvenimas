2e99f8285122942392417b7c40c13a17f39fe0a7
martynux123 <dargis.martynas@gmail.com>
2016-09-10 23:29:40 +0300

    google maps initialized

--- sveikasGyvenimas/app/src/main/java/com/sveikata/productions/mabe/sveikasgyvenimas/HealthyLifeActivity.java
+++ sveikasGyvenimas/app/src/main/java/com/sveikata/productions/mabe/sveikasgyvenimas/HealthyLifeActivity.java
@@ -5,13 +5,26 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class HealthyLifeActivity extends android.support.v4.app.Fragment {
+
+    private GoogleMap googleMaps;
+    private SupportMapFragment mapfragment;
 
     @Nullable
     @Override
@@ -28,10 +41,39 @@ public class HealthyLifeActivity extends android.support.v4.app.Fragment {
         super.onActivityCreated(savedInstanceState);
 
         FragmentManager fm = getChildFragmentManager();
-        Fragment fragment = (SupportMapFragment) fm.findFragmentById(R.id.map_container);
-        if (fragment == null) {
-            fragment = SupportMapFragment.newInstance();
-            fm.beginTransaction().replace(R.id.map_container, fragment).commit();
+        mapfragment = (SupportMapFragment) fm.findFragmentById(R.id.map_container);
+        if (mapfragment == null) {
+            mapfragment = SupportMapFragment.newInstance();
+            fm.beginTransaction().replace(R.id.map_container, mapfragment).commit();
+
+            mapfragment.getMapAsync(new OnMapReadyCallback() {
+                @Override
+                public void onMapReady(GoogleMap googleMap) {
+                    googleMaps = googleMap;
+                    initilizeMap();
+                }
+            });
+
         }
+    }
+
+    private void initilizeMap() {
+            googleMaps.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            googleMaps.getUiSettings().setMyLocationButtonEnabled(true);
+            googleMaps.getUiSettings().setCompassEnabled(true);
+            googleMaps.getUiSettings().setRotateGesturesEnabled(true);
+
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(new LatLng(55.5, 23.7)).zoom(6.25f).build();
+            googleMaps.animateCamera(CameraUpdateFactory
+                    .newCameraPosition(cameraPosition));
+
+            // check if map is created successfully or not
+            if (googleMaps == null) {
+                Toast.makeText(HealthyLifeActivity.this.getActivity(),
+                        "Atsiprašome! Jums žemėlapis neveikia.", Toast.LENGTH_SHORT)
+                        .show();
+            }
+
     }
 }
