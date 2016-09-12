6ac936b466b931e530d57da68ae70ac279c8c1f4
martynux123 <dargis.martynas@gmail.com>
2016-09-12 19:17:40 +0300

    FAIL

--- sveikasGyvenimas/app/src/main/java/com/sveikata/productions/mabe/sveikasgyvenimas/HealthyLifeActivity.java
+++ sveikasGyvenimas/app/src/main/java/com/sveikata/productions/mabe/sveikasgyvenimas/HealthyLifeActivity.java
@@ -97,46 +97,7 @@ public class HealthyLifeActivity extends android.support.v4.app.Fragment {
 
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-        FragmentManager fm = getChildFragmentManager();
-        mapfragment = (SupportMapFragment) fm.findFragmentById(R.id.map_container);
-        if (mapfragment == null) {
-            mapfragment = SupportMapFragment.newInstance();
-            fm.beginTransaction().replace(R.id.map_container, mapfragment).commit();
-
-            mapfragment.getMapAsync(new OnMapReadyCallback() {
-                @Override
-                public void onMapReady(GoogleMap googleMap) {
-                    googleMaps = googleMap;
-                    initilizeMap();
-                }
-            });
-
-        }
-    }
-
-    private void initilizeMap() {
-            googleMaps.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-            googleMaps.getUiSettings().setMyLocationButtonEnabled(true);
-            googleMaps.getUiSettings().setCompassEnabled(true);
-            googleMaps.getUiSettings().setRotateGesturesEnabled(true);
-
-            CameraPosition cameraPosition = new CameraPosition.Builder()
-                    .target(new LatLng(55.5, 23.7)).zoom(6.25f).build();
-            googleMaps.animateCamera(CameraUpdateFactory
-                    .newCameraPosition(cameraPosition));
-
-            // check if map is created successfully or not
-            if (googleMaps == null) {
-                Toast.makeText(HealthyLifeActivity.this.getActivity(),
-                        "Atsiprašome! Jums žemėlapis neveikia.", Toast.LENGTH_SHORT)
-                        .show();
-            }
-
-    }
+    
     public void initializeData(RecyclerAdapter adapter){
         //Preferences to fetch all schedule data
         SharedPreferences dataPrefs = getActivity().getSharedPreferences("ScheduleData", getActivity().MODE_PRIVATE);
