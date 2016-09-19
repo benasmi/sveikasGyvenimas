d2fca14d71470bf383806f57790d73c705b0dad8
pijoneris <benasmiliunas@gmail.com>
2016-09-19 17:38:49 +0300

    ssss

--- sveikasGyvenimas/app/src/main/java/com/sveikata/productions/mabe/sveikasgyvenimas/AskQuestionsActivity.java
+++ sveikasGyvenimas/app/src/main/java/com/sveikata/productions/mabe/sveikasgyvenimas/AskQuestionsActivity.java
@@ -138,9 +138,19 @@ public class AskQuestionsActivity extends android.support.v4.app.Fragment{
         SharedPreferences dataPrefs = getActivity().getSharedPreferences("QuestionsData", getActivity().MODE_PRIVATE);
         String schedule = dataPrefs.getString("questions_data", "");
 
-        adapter.add(new QuestionsDataHolder("Ar galima vairuoti išgėrus?", "Taip, tai yra patartina. Taip pat parūkykite prieš vairuodami."), 0);
-        adapter.add(new QuestionsDataHolder("Ar man eiti miegoti?", "Ne, geriau taip nedaryti. Nors px man, daryk jei nori."), 0);
-        adapter.add(new QuestionsDataHolder("Kokias kojines mautis?", "Nesimauk. Patartina vaikščioti be kojinių."), 0);
+        adapter.add(new QuestionsDataHolder("Ko siekiama projektu?", "Vykdyti alkoholio vartojimo prevenciją tarp vaikų ir jaunimo, įtraukiant jų tėvus (teisėtus vaiko atstovus pagal įstatymą), ugdymo įstaigas ir kitus bendruomenės narius."), 0);
+        adapter.add(new QuestionsDataHolder("Kam skirtas šis projektas?", "Vaikams ir jaunimui, tėvams, vietos bendruomenių nariams, kitoms gyventojų grupėms."), 0);
+        adapter.add(new QuestionsDataHolder("Kas įgyvendina projektą?", "Projektą įgyvendina Viešoji įstaiga „Darnaus vystymo projektai“ kartu su partneriais."), 0);
+        adapter.add(new QuestionsDataHolder("Kas finansuoja projektą?", "Projektas finansuojamas Valstybinio visuomenės sveikatos stiprinimo fondo lėšomis."), 0);
+        adapter.add(new QuestionsDataHolder("Kuriose savivaldybėse vyks projekto renginiai?", "Preliminariai Rietavo r., Kupiškio r., Molėtų r., Biržų r., Anykščių r., Kalvarijos, Prienų r., Šalčininkų r., Trakų r., Pasvalio r., Pagėgių r., Kelmės r., Širvintų r., Rokiškio r., Ignalinos r., Ukmergės r., Zarasų r., Alytaus r., Panevėžio r., Kaišiadorių r., Varėnos r., Lazdijų r., Vilkaviškio r., Kėdainių r., Telšių r., Šilalės r., Raseinių r., Švenčionių r., Jurbarko r., Kretingos r."), 0);
+        adapter.add(new QuestionsDataHolder("Projekto įgyvendinimo trukmė?", "Tai 12 mėn. projektas."), 0);
+        adapter.add(new QuestionsDataHolder("Kas įgyvendina projektą?", "Projektą įgyvendina Viešoji įstaiga „Darnaus vystymo projektai“ kartu su partneriais.."), 0);
+        adapter.add(new QuestionsDataHolder("Koks projekto tikslas?", "Skatinti vaikus, jaunimą, kitas gyventojų grupes atsisakyti alkoholio ir sveikiau gyventi."), 0);
+        adapter.add(new QuestionsDataHolder("Kas vyks šio projekto metu?", "•\tBus sukurtas puslapis socialiniame tinkle „Facebook“ ir nemokama interaktyvi Android mobili aplikacija apie sveikatą, sveiką gyvenseną, alkoholio daromą žalą. \n•\tProjekte dalyvausiančiose savivaldybėse bus parengti Sveikos gyvensenos ambasadoriai, kurie padės vykdyti alkoholio prevencijos bei sveikos gyvensenos renginius savivaldybėse, kitas projekto veiklas. \n•\tProjekte dalyvausiančiose savivaldybėse (mokyklose, atviruose jaunimo centruose, masiniuose renginiuose ir kitur) bus vykdomi alkoholio prevencijos per sveikos gyvensenos prizmę renginiai.\n•\tProjekte dalyvausiančiose savivaldybėse bus vykdomas marškinėlių maketavimo konkursas „Sveikas gyvenimas – blaivus gyvenimas!“."), 0);
+        adapter.add(new QuestionsDataHolder("Kas yra Sveikos gyvensenos ambasadorius?", "Sveikos gyvensenos ambasadorius – tai asmuo, kuris rūpinsis projekto renginių ir konsultacijų organizavimu bei padės kiekvienam Projekto dalyviui suvokti savo asmenines galimybes, padės išlikti ryžtingam ugdant sveikos gyvensenos įpročius. "), 0);
+        adapter.add(new QuestionsDataHolder("Kas įgyvendina projektą?", "Projektą įgyvendina Viešoji įstaiga „Darnaus vystymo projektai“ kartu su partneriais.."), 0);
+        adapter.add(new QuestionsDataHolder("Kaip galėčiau sužinoti apie mano mieste vykstančius renginius?", "Daugiau apie renginius tavo mieste žiūrėti 'Tvarkaraštis' skiltyje."), 0);
+        adapter.add(new QuestionsDataHolder("Kaip galėčiau prisidėti prie projekto įgyvendinimo?", "Tapk projekto socialiniu partneriu, tapk projekto dalyviu.\n" + "Parašyk mums: info@dvp.lt ir tap mūsų komandos nariu.\n"), 0);
 
     }
     public void sendEmail(String[] to, String subject, String message){
