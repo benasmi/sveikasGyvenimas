fedea6322ee068ea9df7b40bc5c368bf142aa2cc
martynux123 <dargis.martynas@gmail.com>
2016-09-19 20:17:57 +0300

    adapter facts

---
+++ sveikasGyvenimas/app/src/main/java/com/sveikata/productions/mabe/sveikasgyvenimas/FactInfoHolder.java
@@ -0,0 +1,52 @@
+package com.sveikata.productions.mabe.sveikasgyvenimas;
+
+/**
+ * Created by Martyno on 2016.09.19.
+ */
+
+public class FactInfoHolder {
+
+    private String fact_title;
+    private String fact_body;
+    private String fact_image_url;
+    private String fact_source;
+
+    public FactInfoHolder(String title, String body, String image_URL, String source){
+        this.fact_title = title;
+        this.fact_body = body;
+        this.fact_image_url = image_URL;
+        this.fact_source = source;
+    }
+
+    public String getFactTitle() {
+        return fact_title;
+    }
+
+    public void setFactTitle(String fact_title) {
+        this.fact_title = fact_title;
+    }
+
+    public String getFactBody() {
+        return fact_body;
+    }
+
+    public void setFactBody(String fact_body) {
+        this.fact_body = fact_body;
+    }
+
+    public String getFactImageUrl() {
+        return fact_image_url;
+    }
+
+    public void setFactImageUrl(String fact_image_url) {
+        this.fact_image_url = fact_image_url;
+    }
+
+    public String getFactSource() {
+        return fact_source;
+    }
+
+    public void setFactSource(String fact_source) {
+        this.fact_source = fact_source;
+    }
+}
